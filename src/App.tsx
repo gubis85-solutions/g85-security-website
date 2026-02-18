cbc9d74bc6ff81299a6a7013c4208a6ff3474ecc
pbooi70 <booiprince0@example.com>
2026-02-18 09:08:16 +0200

    Fixed rerouting issue

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <Navigation />
       <main className="main-content">
